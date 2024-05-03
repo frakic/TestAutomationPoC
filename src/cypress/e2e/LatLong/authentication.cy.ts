50ea8426945f90a217ee838447070b1f0b4ea465
frakic <frakic@frakic.com>
2024-05-03 09:44:14 +0200

    add logout test

--- src/cypress/e2e/LatLong/authentication.cy.ts
+++ src/cypress/e2e/LatLong/authentication.cy.ts
@@ -1,34 +1,48 @@
-import { UserLoginPage } from '../../pages'
+import { UserAccountPage, UserLoginPage } from 'pages'
 
 describe('Authentication', () => {
   const userLoginPage = new UserLoginPage()
+  const userAccountPage = new UserAccountPage()
 
   const email = Cypress.env('email')
   const password = Cypress.env('password')
 
-  beforeEach(() => {
-    userLoginPage.visit()
+  context('login', () => {
+    beforeEach(() => {
+      userLoginPage.visit()
+    })
+
+    it('redirects to the user homepage after successful login', () => {
+      userLoginPage.fillEmail(email)
+      userLoginPage.fillPassword(password)
+      userLoginPage.clickSubmit()
+    })
+
+    it('displays an error when the password is incorrect', () => {
+      const password = 'wrong123!'
+
+      userLoginPage.fillEmail(email)
+      userLoginPage.fillPassword(password)
+      userLoginPage.clickSubmit({ incorrectPassword: true })
+    })
+
+    it('displays an error when the password is too short', () => {
+      const password = 'short'
+
+      userLoginPage.fillEmail(email)
+      userLoginPage.fillPassword(password)
+      userLoginPage.clickSubmit({ shortPassword: true })
+    })
   })
 
-  it('redirects to the user homepage after successful login', () => {
-    userLoginPage.fillEmail(email)
-    userLoginPage.fillPassword(password)
-    userLoginPage.clickSubmit()
-  })
+  context('logout', () => {
+    before(() => {
+      cy.login(email, password)
+      userAccountPage.visit()
+    })
 
-  it('displays an error when the password is incorrect', () => {
-    const password = 'wrong123!'
-
-    userLoginPage.fillEmail(email)
-    userLoginPage.fillPassword(password)
-    userLoginPage.clickSubmit({ incorrectPassword: true })
-  })
-
-  it('displays an error when the password is too short', () => {
-    const password = 'short'
-
-    userLoginPage.fillEmail(email)
-    userLoginPage.fillPassword(password)
-    userLoginPage.clickSubmit({ shortPassword: true })
+    it('redirects to the user login screen after successful logout', () => {
+      userAccountPage.clickLogoutLink()
+    })
   })
 })
